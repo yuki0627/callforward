d4114b58f19c22d3e5cbacda9b00968dd530ae8f
Yuki Takahashi <yuki0627@gmail.com>
2015-12-06 21:22:19 +0000

    for heroku

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,8 +1,8 @@
 class CallsController < ApplicationController
   def forward
     render text: "<Response><Dial callerId=\"+815031774622\">+819053909417</Dial></Response>"
-    @forward_call = ForwardCall.new
-    @forward_call.phone_number = params[:Caller]
-    @forward_call.save
+    @call = Call.new
+    @call.phone_number = params[:Caller]
+    @call.save
   end
 end
