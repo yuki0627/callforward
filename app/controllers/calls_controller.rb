246c6e2fb81ad9cf3204fd83d795109c97df5122
Yuki Takahashi <yuki0627@gmail.com>
2016-08-10 13:12:21 +0000

    summer 対応

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,13 +1,13 @@
 class CallsController < ApplicationController
   def forward
     caller = params[:Caller]
-    # render text: "<Response>
-    #                 <Say voice='woman' language='ja-JP'>申し訳ございません。夏季休暇中です。営業再開は８月１７日になります。</Say>
-    #                 <Dial callerId=\"+815031774622\">+819053909417</Dial>
-    #               </Response>"
     render text: "<Response>
+                    <Say voice='woman' language='ja-JP'>申し訳ございません。夏季休暇中です。営業再開は８月１７日になります。</Say>
                     <Dial callerId=\"+815031774622\">+819053909417</Dial>
                   </Response>"
+    # render text: "<Response>
+    #                 <Dial callerId=\"+815031774622\">+819053909417</Dial>
+    #               </Response>"
     call = Call.new
     call.phone_number = caller
     call.save
