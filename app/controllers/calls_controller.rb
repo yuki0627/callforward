fb399c3b78256e529e600321624ff6fd60fc06c2
Yuki Takahashi <yuki0627@gmail.com>
2016-08-17 01:37:00 +0000

    summer 対応解除

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,12 +1,12 @@
 class CallsController < ApplicationController
   def forward
     caller = params[:Caller]
-    render text: "<Response>
-                    <Say voice='woman' language='ja-JP'>申し訳ございません。営業再開は８月１７日になります。よろしくお願いします</Say>
-                  </Response>"
     # render text: "<Response>
-    #                 <Dial callerId=\"+815031774622\">+819053909417</Dial>
+    #                 <Say voice='woman' language='ja-JP'>申し訳ございません。営業再開は８月１７日になります。よろしくお願いします</Say>
     #               </Response>"
+    render text: "<Response>
+                    <Dial callerId=\"+815031774622\">+819053909417</Dial>
+                  </Response>"
     call = Call.new
     call.phone_number = caller
     call.save
