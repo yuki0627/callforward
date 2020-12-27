5194fed13cb585682920af661308995172097c1a
Yuki Takahashi <yuki0627@gmail.com>
2020-12-27 09:43:29 +0900

    fix: 電話番号変更の為、転送先変更

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -5,7 +5,7 @@ class CallsController < ApplicationController
     #                 <Say voice='woman' language='ja-JP'>申し訳ございません。営業再開は８月１７日になります。よろしくお願いします</Say>
     #               </Response>"
     render text: "<Response>
-                    <Dial callerId=\"+815031774622\">+819053909417</Dial>
+                    <Dial callerId=\"+815031774622\">+818067987319</Dial>
                   </Response>"
     call = Call.new
     call.phone_number = caller
